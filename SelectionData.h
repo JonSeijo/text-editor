15514df57ebc464b8a897a3b02f44c789e424f9b
jonseijo <jon.seijo@gmail.com>
2017-04-23 17:39:56 -0300

    Add SelectionData bone class

---
+++ SelectionData.h
@@ -0,0 +1,32 @@
+#ifndef SelectionData_H
+#define SelectionData_H
+
+#include <iostream>
+#include <vector>
+
+class SelectionData {
+
+    public:
+        void addSelection(int start, int end);
+
+        void removeSelection(int index);
+        void removeSelection(int start, int end);
+
+        int getStart(int index);
+        int getEnd(int index);
+
+    private:
+        struct Selection {
+            int start;
+            int end;
+
+            Selection() : start(-1), end(-1) {}
+            Selection(int start, int end) : start(start), end(end) {}
+
+        };
+        std::vector<Selection> selections;
+        bool validIndex(int index);
+};
+
+
+#endif
