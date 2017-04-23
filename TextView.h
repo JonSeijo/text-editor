23cb6f07bc29efed32ea8bd741dd0b751225be34
jonseijo <jon.seijo@gmail.com>
2017-04-23 10:50:07 -0300

    Create new TextView interface

---
+++ TextView.h
@@ -0,0 +1,41 @@
+#ifndef TextView_H
+#define TextView_H
+
+#include <SFML/Graphics.hpp>
+#include "TextViewContent.h"
+#include "TextViewCamera.h"
+
+class TextView {
+
+    public:
+        TextView(const sf::RenderWindow &window);
+
+        void drawLines(sf::RenderWindow &window, TextDocument &document);
+        void setFontSize(int fontSize);
+
+        void scrollUp();
+        void scrollDown();
+        void scrollLeft();
+        void scrollRight();
+
+        void scroll(float deltaX, float deltaY);
+        void scrollTo(float x, float y);
+
+        void rotateLeft();
+        void rotateRight();
+
+        sf::View getCameraView();
+
+        void setDeltaScroll(float delta);
+        void setDeltaRotation(float delta);
+
+
+    private:
+        TextViewContent content;
+
+        sf::View camera;
+        float deltaScroll;
+        float deltaRotation;
+};
+
+#endif
