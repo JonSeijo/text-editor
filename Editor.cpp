585a1381dc3280e287a83907ca24a1539bccc6b8
jonseijo <jon.seijo@gmail.com>
2017-04-25 17:08:36 -0300

    Expand mouse control

--- Editor.cpp
+++ Editor.cpp
@@ -5,16 +5,16 @@
 void controlarMovimientos(TextView &textView) {
     // Scrolling
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        textView.scrollUp();
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
         textView.scrollDown();
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        textView.scrollUp();
+    }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        textView.scrollLeft();
+        textView.scrollRight();
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        textView.scrollRight();
+        textView.scrollLeft();
     }
 
     // Rotating
@@ -28,19 +28,29 @@ void controlarMovimientos(TextView &textView) {
             }
         }
     }
+}
 
+void controlarAccionesMouse(sf::RenderWindow &window, TextView &textView, bool isMouseDown) {
+    if (isMouseDown) {
+        auto mousepos = sf::Mouse::getPosition(window);
+        auto mousepos_text = window.mapPixelToCoords(mousepos);
+        textView.mouseClick(mousepos_text.x, mousepos_text.y);
+    }
 }
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(720, 405), "text-editor", sf::Style::Close);
     window.setVerticalSyncEnabled(true);
 
-    std::string fileName = "textoDePrueba.txt";
+    // std::string fileName = "textoDePrueba.txt";
+    std::string fileName = "TextView.cpp";
 
     TextView textView(window);
     TextDocument doc;
 
     doc.init(fileName);
+
+    bool isMouseDown = false;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -50,11 +60,15 @@ int main() {
             }
 
             if (event.type == sf::Event::MouseButtonPressed) {
+                textView.removeSelections();
                 auto mousepos = sf::Mouse::getPosition(window);
                 auto mousepos_text = window.mapPixelToCoords(mousepos);
-                std::cout << "click: \n";
-
                 textView.mouseClick(mousepos_text.x, mousepos_text.y);
+                isMouseDown = true;
+            }
+
+            if (event.type == sf::Event::MouseButtonReleased) {
+                isMouseDown = false;
             }
 
             if (event.type == sf::Event::KeyPressed) {
@@ -71,6 +85,7 @@ int main() {
         }
 
         controlarMovimientos(textView);
+        controlarAccionesMouse(window, textView, isMouseDown);
 
         window.clear();
 
