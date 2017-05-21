cda663d0b90d0bb0c8c8e20157ff1399936bc7c6
jonseijo <jon.seijo@gmail.com>
2017-05-21 13:03:09 -0300

    Add method to get the current directory

---
+++ src/ImplementationUtils.cpp
@@ -0,0 +1,11 @@
+#include "ImplementationUtils.h"
+
+std::string ImplementationUtils::getWorkingDirectory(const std::string &argvZero) {
+    std::string directory(argvZero);
+    std::string execName("editor");
+
+    // Remove the executable name from the directory
+    directory.erase(directory.size()-execName.size(), execName.size());
+
+    return directory;
+}
---
+++ src/ImplementationUtils.h
@@ -0,0 +1,13 @@
+#ifndef ImplementationUtils_H
+#define ImplementationUtils_H
+
+#include <string>
+
+class ImplementationUtils {
+
+    public:
+        static std::string getWorkingDirectory(const std::string &argvZero);
+};
+
+
+#endif
